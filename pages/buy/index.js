69c285cec4e7f8ede052467863d852d72ba9b942
Sachin Yadav <sachin.yadav@csinfocomm.com>
2022-04-27 22:14:24 +0530

    Initial commit

---
+++ pages/buy/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Buy = () => {
+  return (
+    <div>Buy Page</div>
+  )
+}
+
+export default Buy
---
+++ pages/buy/property/[city_name]/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CityName = () => {
+  return (
+    <div>CityName</div>
+  )
+}
+
+export default CityName
